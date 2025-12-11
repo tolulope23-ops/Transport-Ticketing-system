dfbeede8dfb8b7c2e598b4d483605ba5fa7ea344
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2025-12-12 00:27:43 +0100

    update ticket table columns

--- model/sql.js
+++ model/sql.js
@@ -1,5 +1,5 @@
 const sqlQuery = {
-    createTicket: 'INSERT INTO Ticket(passenger_name, ticket_code, amount, seat_number, weather, park_location, destinantion, date_expired) VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
+    createTicket: 'INSERT INTO Ticket(passenger_name, ticket_code, amount, seat_number, weather, park_location, destination, date_expired) VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
     getTicket: 'SELECT * FROM Ticket',
     getSeatNumber: "SELECT * FROM ticket WHERE seat_number = ?"
 }
